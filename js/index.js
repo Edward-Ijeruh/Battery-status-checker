f5ca455c4f4b7ac647c37f86ad854d8c0f248c38
Edward Ijeruh <zacharaiah17@gmail.com>
2022-08-22 08:22:26 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let chargingState = document.getElementById("charging-state");
+let chargingStatus = document.getElementById("charging-status");
+
+window.navigator.getBattery()
+    .then(batteryManager => {
+        const updateLevel = () => {
+            chargingState.textContent = `${batteryManager.level * 100}%`
+        }
+        updateLevel();
+        const updateStatus = () => {
+            if (batteryManager.charging === true) {
+                chargingStatus.textContent = "Charging";
+                chargingStatus.style.color = "green";
+            } else {
+                chargingStatus.textContent = "Not charging";
+                chargingStatus.style.color = "red";
+            }
+        }
+        updateStatus();
+        batteryManager.addEventListner("chargingchange", updateStatus());
+        batteryManager.addEventListner("levelchange", updateLevel());
+        
+    });
+
+        
+    
